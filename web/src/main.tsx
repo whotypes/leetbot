6a0380977c0885f5ce377215f3790d1bad24f745
whotypes <titandq@gmail.com>
2025-11-24 00:46:18 -0800

    remove strict mode from the app entry

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Providers } from './providers'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <Providers>
       <App />
-    </Providers>
-  </StrictMode>,
+  </Providers>,
 )
